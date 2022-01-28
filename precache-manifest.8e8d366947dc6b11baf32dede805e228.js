a908fed1fa406d98d3778927129864d08ea1df6a
atenobi <79144475+atenobi@users.noreply.github.com>
2022-01-28 20:10:52 +0300

    Updates

---
+++ precache-manifest.8e8d366947dc6b11baf32dede805e228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e539dc905b34d95ae5e6eefd0ea7745e",
+    "url": "/test-task-todo/index.html"
+  },
+  {
+    "revision": "4b3665f03b5c3cdc681d",
+    "url": "/test-task-todo/static/css/main.f52dba9e.chunk.css"
+  },
+  {
+    "revision": "e35d668a4ca3cde53495",
+    "url": "/test-task-todo/static/js/2.3c21c266.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/test-task-todo/static/js/2.3c21c266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3665f03b5c3cdc681d",
+    "url": "/test-task-todo/static/js/main.1a6496ab.chunk.js"
+  },
+  {
+    "revision": "e00069907c5dc3cb80b6",
+    "url": "/test-task-todo/static/js/runtime-main.db806d7c.js"
+  },
+  {
+    "revision": "78709f14495a7df560b2839530475aec",
+    "url": "/test-task-todo/static/media/todo-listl-background.78709f14.jpg"
+  }
+]);
